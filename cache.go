06c0d86cde8a74ffb92ffd33ce308b6c281cee50
utkuxgok <113059395+utkuxgok@users.noreply.github.com>
2023-04-06 04:44:03 +0300

    Update cache.go

--- cache.go
+++ cache.go
@@ -1,55 +1,46 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
+	"github.com/adshao/go-binance/v2"
 	"github.com/redis/go-redis"
 	"os"
 	"time"
 )
 
-type Candlestick struct {
-	OpenTime                 time.Time
-	Open, High, Low, Close   float64
-	Volume                   float64
-	CloseTime                time.Time
-	QuoteAssetVolume         float64
-	TakerBuyBaseAssetVolume  float64
-	TakerBuyQuoteAssetVolume float64
-	SMA10, SMA30             float64
-	RSI14                    float64
-	Returns                  float64
+type Kline struct {
+	Symbol string
+	Kline  *binance.WsKline
 }
 
-type OrderBook struct {
-	Bids []OrderBookEntry
-	Asks []OrderBookEntry
+type AggTrade struct {
+	Symbol   string
+	AggTrade *binance.WsAggTradeEvent
 }
 
-type OrderBookEntry struct {
-	Price    float64
-	Quantity float64
-}
-
-type Trade struct {
-	ID               int64
-	Price            float64
-	Quantity         float64
-	BuyerIsMaker     bool
-	Time             int64
-	IsBestPriceMatch bool
+type Depth struct {
+	Symbol string
+	Depth  *binance.WsDepthEvent
 }
 
 type Cache struct {
 	client *redis.Client
 }
 
+const (
+	KlineKeyPrefix    = "kline:"
+	TradeKeyPrefix    = "trade:"
+	DepthKeyPrefix    = "depth:"
+	KlineCacheMaxSize = 10000
+	TradeCacheMaxSize = 100000
+	DepthCacheMaxSize = 100000
+)
+
 func NewCache() *Cache {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"), // e.g. "localhost:6379"
-		Password: os.Getenv("REDIS_PASS"), // no password set by default
-		DB:       0,                       // use default DB
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASS"),
+		DB:       0,
 	})
 
 	return &Cache{
@@ -63,7 +54,12 @@ func (c *Cache) Set(key string, value interface{}, expiration time.Duration) err
 		return err
 	}
 
-	err = c.client.Set(context.Background(), key, data, expiration).Err()
+	// Use a Redis pipeline to optimize I/O
+	pipe := c.client.Pipeline()
+	pipe.Set(key, data, expiration)
+	pipe.LPush(key+":list", data)
+	pipe.LTrim(key+":list", 0, KlineCacheMaxSize-1)
+	_, err = pipe.Exec()
 	if err != nil {
 		return err
 	}
@@ -72,7 +68,7 @@ func (c *Cache) Set(key string, value interface{}, expiration time.Duration) err
 }
 
 func (c *Cache) Get(key string, target interface{}) (bool, error) {
-	data, err := c.client.Get(context.Background(), key).Bytes()
+	data, err := c.client.Get(key).Bytes()
 	if err != nil {
 		if err == redis.Nil {
 			return false, nil
@@ -89,32 +85,159 @@ func (c *Cache) Get(key string, target interface{}) (bool, error) {
 }
 
 func (c *Cache) Delete(key string) error {
-	err := c.client.Del(context.Background(), key).Err()
+	err := c.client.Del(key).Err()
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (c *Cache) UpdateKlines(symbol string, kline Candlestick) error {
-	key := fmt.Sprintf("klines:%s", symbol)
+func (c *Cache) UpdateKline(symbol string, klineData *Kline, expiration time.Duration) error {
+	key := KlineKeyPrefix + symbol
 
-	found, err := c.Get(key, &kline)
+	// Get the existing klines for the symbol from the cache
+	var klines []*Kline
+	_, err := c.Get(key, &klines)
 	if err != nil {
 		return err
 	}
 
-	var klines []Candlestick
-	if found {
-		klines = append(klines, kline)
-	} else {
-		klines = []Candlestick{kline}
+	// Append the new kline to the list
+	klines = append(klines, klineData)
+
+	// If the list is too long, remove the oldest kline
+	if len(klines) > KlineCacheMaxSize {
+		klines = klines[len(klines)-KlineCacheMaxSize:]
 	}
 
-	err = c.Set(key, klines, 24*time.Hour)
+	// Update the cache with the new klines
+	err = c.Set(key, klines, expiration)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (c *Cache) UpdateTrade(symbol string, tradeData *AggTrade, expiration time.Duration) error {
+	key := TradeKeyPrefix + symbol
+
+	// Get the existing trades for the symbol from the cache
+	var trades []*AggTrade
+	_, err := c.Get(key, &trades)
+	if err != nil {
+		return err
+	}
+
+	// Append the new trade to the list
+	trades = append(trades, tradeData)
+
+	// If the list is too long, remove the oldest trade
+	if len(trades) > TradeCacheMaxSize {
+		trades = trades[len(trades)-TradeCacheMaxSize:]
+	}
+
+	// Update the cache with the new trades
+	err = c.Set(key, trades, expiration)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Cache) UpdateDepth(symbol string, depthData *Depth, expiration time.Duration) error {
+	key := DepthKeyPrefix + symbol
+
+	// Get the existing depth data for the symbol from the cache
+	var depths []*Depth
+	_, err := c.Get(key, &depths)
+	if err != nil {
+		return err
+	}
+
+	// Append the new depth data to the list
+	depths = append(depths, depthData)
+
+	// If the list is too long, remove the oldest depth data
+	if len(depths) > DepthCacheMaxSize {
+		depths = depths[len(depths)-DepthCacheMaxSize:]
+	}
+
+	// Update the cache with the new depth data
+	err = c.Set(key, depths, expiration)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Cache) GetKlines(symbol string, limit int) ([]*Kline, error) {
+	key := KlineKeyPrefix + symbol
+
+	// Get the klines for the symbol from the cache
+	var klines []*Kline
+	found, err := c.Get(key, &klines)
+	if err != nil {
+		return nil, err
+	}
+
+	// If the klines were not found in the cache, return an empty slice
+	if !found {
+		return []*Kline{}, nil
+	}
+
+	// Return the last `limit` klines
+	if len(klines) > limit {
+		klines = klines[len(klines)-limit:]
+	}
+
+	return klines, nil
+}
+
+func (c *Cache) GetTrades(symbol string, limit int) ([]*AggTrade, error) {
+	key := TradeKeyPrefix + symbol
+
+	// Get the trades for the symbol from the cache
+	var trades []*AggTrade
+	found, err := c.Get(key, &trades)
+	if err != nil {
+		return nil, err
+	}
+
+	// If the trades were not found in the cache, return an empty slice
+	if !found {
+		return []*AggTrade{}, nil
+	}
+
+	// Return the last `limit` trades
+	if len(trades) > limit {
+		trades = trades[len(trades)-limit:]
+	}
+
+	return trades, nil
+}
+
+func (c *Cache) GetDepth(symbol string, limit int) ([]*Depth, error) {
+	key := DepthKeyPrefix + symbol
+
+	// Get the depth data for the symbol from the cache
+	var depths []*Depth
+	found, err := c.Get(key, &depths)
+	if err != nil {
+		return nil, err
+	}
+
+	// If the depth data was not found in the cache, return an empty slice
+	if !found {
+		return []*Depth{}, nil
+	}
+
+	// Return the last `limit` depth data
+	if len(depths) > limit {
+		depths = depths[len(depths)-limit:]
+	}
+
+	return depths, nil
 }
